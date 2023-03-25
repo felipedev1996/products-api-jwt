d2e894a9c3a186c6b6089c182df758e72ef2c613
felipedev1996 <felipepino1996@gmail.com>
2023-03-25 11:23:17 -0300

    api ready for build, repair details performance

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+config();
+
+export const MONGODB_URI =process.env.MONGODB_URI || "mongodb://localhost/apicompany";
+export const PORT = process.env.PORT || 4000;
+export const SECRET = "yoursecretkey";
+
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "products@gmail.com";
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME || "sysadmin";
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
